6072d16b5032e661a6cda0384d34542a0ae629df
bagus <bwibowo122@gmail.com>
2022-08-19 22:48:01 +0700

    Kodingan FIX BISA

--- hand_control.py
+++ hand_control.py
@@ -2,31 +2,28 @@ import cv2
 import time
 import numpy as np
 import HandTrackingModule as htm
-import math
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
 
 ################################
 wCam, hCam = 640, 480
 ################################
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(3, wCam)
 cap.set(4, hCam)
 pTime = 0
 
 detector = htm.handDetector(detectionCon=0.7, maxHands=1)
 
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
+#devices = AudioUtilities.GetSpeakers()
+#interface = devices.Activate(
+    #IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+#volume = cast(interface, POINTER(IAudioEndpointVolume))
 # volume.GetMute()
 # volume.GetMasterVolumeLevel()
-volRange = volume.GetVolumeRange()
-minVol = volRange[0]
-maxVol = volRange[1]
+#volRange = volume.GetVolumeRange()
+#minVol = volRange[0]
+#maxVol = volRange[1]
 vol = 0
 volBar = 400
 volPer = 0
@@ -75,8 +72,8 @@ while True:
     cv2.rectangle(img, (50, int(volBar)), (85, 400), (255, 0, 0), cv2.FILLED)
     cv2.putText(img, f'{int(volPer)} %', (40, 450), cv2.FONT_HERSHEY_COMPLEX,
                 1, (255, 0, 0), 3)
-    cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
-    cv2.putText(img, f'Vol Set: {int(cVol)}', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
+    #cVol = int(volume.GetMasterVolumeLevelScalar() * 100)
+    cv2.putText(img, f'Vol Set: {int(volPer)} %', (400, 50), cv2.FONT_HERSHEY_COMPLEX,
                 1, colorVol, 3)
 
     # Frame rate
